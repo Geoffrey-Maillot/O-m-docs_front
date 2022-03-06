ad52672bd13cb6c30335683641617e8e7a270741
geoffrey <geoffrey.maillot@gmail.com>
2022-03-06 15:55:32 +0100

    Add headers 'Access-Control-Allow-Origin

--- src/api/api.js
+++ src/api/api.js
@@ -3,4 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: process.env.BASE_URL_HEROKU,
   timeout: 3000,
+  heraders: {
+    'Access-Control-Allow-Origin': '*',
+  },
 });
